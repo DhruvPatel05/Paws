f6cdf94efbf9411d8653cd5eef32ed9074fcd1c7
Dhruv Patel <dhruvpatel913@gmail.com>
2026-05-29 17:58:08 -0700

    GridLayout added

--- Paws/Paws/ContentView.swift
+++ Paws/Paws/ContentView.swift
@@ -11,12 +11,51 @@ import SwiftData
 struct ContentView: View {
     @Environment(\.modelContext) private var modelContext
     @Query private var pets: [Pet]
+    
+    let layout = [
+        GridItem(.flexible(minimum: 120)),
+        GridItem(.flexible(minimum: 120))
+        
+    ]
 
     var body: some View {
         NavigationStack {
             ScrollView{
-                EmptyView()
+                LazyVGrid(columns: layout){
+                    GridRow {
+                        ForEach(pets) { pet in
+                            NavigationLink(destination:EmptyView()) {
+                                VStack {
+                                    if let imagedata = pet.photo {
+                                        if let petImage = UIImage(data: imagedata) {
+                                Image(uiImage: petImage)
+                                            
+                                        }
+                                        
+                                    } else {
+                                      Image(systemName: "pawprint.circle")
+                                            .resizable()
+                                            .scaledToFit()
+                                            .padding(40)
+                                            .foregroundStyle(.quaternary)
+                                    }
+                                    Spacer()
+                                    Text(pet.name)
+                                        .font(.title.weight(.light))
+                                        .padding(.vertical)
+                                    Spacer()
+                                }//: VStack
+                                .frame(minWidth: 0,maxWidth: .infinity,minHeight:0,maxHeight:.infinity)
+                                .background(.ultraThinMaterial)
+                                .clipShape(RoundedRectangle(cornerRadius: 8,style:.circular))
+                            }
+                            .foregroundStyle(.primary)
+                        }//:Loop
+                    }//:Grid Row
+                }//:Grid layout
+                .padding(.horizontal)
             }// Scrollview
+            .navigationTitle(pets.isEmpty ? "" : "Paws")
             .overlay {
                 if pets.isEmpty {
                     CustomContentUnavailableView(icon:"dog.circle",title: "No Pets",description: "Add a new pet to get started.")
@@ -24,6 +63,11 @@ struct ContentView: View {
             }
         }
     }
+}
+
+#Preview ("Sample Data") {
+    ContentView()
+        .modelContainer(Pet.preview)
 }
 
 #Preview ("No Data"){
